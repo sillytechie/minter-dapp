f4d3c62743093b0ce2290c00e24dbad74b6f38a8
sillytechie <sillytechie@users.noreply.github.com>
2022-03-23 18:32:14 -0700

    update public mint date

--- backend/src/config.js
+++ backend/src/config.js
@@ -64,7 +64,7 @@ const MINT_PRICE = 0.03; // Minting price per NFT. Rinkeby = ETH, Polygon = MATI
 const TOKENS_PER_MINT = 33; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-04-22T09:30:00-08:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2022-03-22T09:30:00-08:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PRESALE_MINT_START_DATE = "2022-03-22T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
